a5e63d9d4b4e6c6493b1ebda68e3c51f35eb0edc
Mónica del Corral <monica.delcorral@wildoom.com>
2020-09-07 18:58:23 +0200

    Add style.js with CKEditor styles

---
+++ style.js
@@ -0,0 +1,49 @@
+(function (blink) {
+	'use strict';
+
+	var BCStyle = function () {
+			blink.theme.styles.basic.apply(this, arguments);
+		},
+		page = blink.currentPage;
+
+	BCStyle.prototype = {
+		parent: blink.theme.styles.basic.prototype,
+		bodyClassName: 'content_type_clase_bc-englishonline',
+		extraPlugins: ['image2'],
+		ckEditorStyles: {
+			name: 'bc-englishonline',
+			styles: [
+
+				{ name: 'TÃ­tulo 1', element: 'h4', attributes: { 'class': 'bck-title1'} },
+				{ name: 'TÃ­tulo 2', element: 'h4', attributes: { 'class': 'bck-title2'} },
+				{ name: 'TÃ­tulo 3', element: 'h4', attributes: { 'class': 'bck-title3'} },
+
+				{ name: 'Ã‰nfasis', element: 'span', attributes: { 'class': 'bck-enfasis' }},
+				{ name: 'Enunciado actividad', element: 'h4', attributes: { 'class': 'bck-title-activity' }},
+
+				{ name: 'Tabla centrada', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-center'} },
+				{ name: 'Celda encabezado', element: 'td', attributes: { 'class': 'bck-td' } },
+
+				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-1' } },
+				{ name: 'Caja 2', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-2' } },
+				{ name: 'Caja 3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-3' } }
+			]
+		},
+
+		init: function (scope) {
+			var that = scope || this;
+			this.parent.init.call(that);
+		},
+
+		removeFinalSlide: function (scope) {
+			var that = scope || this;
+			this.parent.removeFinalSlide.call(that, true);
+		}
+
+	};
+
+	BCStyle.prototype = _.extend({}, new blink.theme.styles.basic(), BCStyle.prototype);
+
+	blink.theme.styles.bcenglishonline = BCStyle;
+
+})( blink );
